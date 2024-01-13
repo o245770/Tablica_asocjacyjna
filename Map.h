28a29306e5786cafcbfa68d19f6859dd0a0afd6a
o245770 <117376822+o245770@users.noreply.github.com>
2024-01-13 20:50:50 +0100

    Stworzenie wzorca i klasy Pair

--- Map.h
+++ Map.h
@@ -1,1 +1,34 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class CK, class CV>
+
+class Map
+{
+private:
+	class Pair
+	{	
+	public:
+		CV value; //empl
+		CK key;  //id
+		Pair(CK k, CV v) : value(v), key(k){};
+		
+		friend ostream& operator << (ostream& stream, const Pair& p) 
+		{
+			stream << p.key<<": "<< p.value;
+			
+			return stream;
+		}
+	};
+	
+	vector<Pair> elements;
+public:
+};
+
+#endif
 
